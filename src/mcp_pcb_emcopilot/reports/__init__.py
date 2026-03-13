071e6da8198e99ee16ec129b6dbf784b535a7bdf
RFingAdam <adam.engelbrecht76@gmail.com>
2026-03-13 14:31:36 -0500

    refactor: make docx_report helpers public, update reports exports

--- src/mcp_pcb_emcopilot/reports/__init__.py
+++ src/mcp_pcb_emcopilot/reports/__init__.py
@@ -1,1 +1,11 @@
-"""Report generation for PCB design reviews."""
+"""PCB design review report generation."""
+
+from .tracked_finding import TrackedFinding
+from .section_registry import REPORT_SECTIONS, SectionDef, get_section_by_key
+
+__all__ = [
+    "TrackedFinding",
+    "REPORT_SECTIONS",
+    "SectionDef",
+    "get_section_by_key",
+]
